4f3bbadc3d560b710304c711c2c04fc36c90d31f
Ashutosh Mishra <ashutoshmmishra15@gmail.com>
2024-12-31 19:47:01 +0530

    Delete java/com/jobportal directory

--- java/com/jobportal/repository/JobSeekerProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jobportal.repository;
-
-import com.jobportal.entity.JobSeekerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile,Integer> {
-}
